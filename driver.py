ee19dcffbda1f4521018b9af49f7a529f4dd1896
benev0 <benjaminevanownby@gmail.com>
2024-08-16 15:57:16 -0400

    gets schema sucessfuly

--- driver.py
+++ driver.py
@@ -1,55 +1,102 @@
+import errno
 import json
 import os
+import subprocess
 import sys
 import requests
 
 validArgs = ["-u", "-f"]
 
+def format_dirname(dir: str):
+        if dir.startswith('./'):
+                return dir[2:]
+        return dir[:]
+
+def init_schema(url: str = "", dir: str = "") -> None:
+        format_dirname(dir)
+        assert dir, "directory name, dir, should not be lambda"
+        assert url, "url should not be lambda"
+
+        ### Acquire schema
+        r = requests.get(url)
+        if r.status_code != 200:
+                print(r.status_code, r.reason)
+                print("request failed")
+                sys.exit(1)
+
+        data = json.loads(r.text)
+
+        ### build file
+        try:
+                os.makedirs(f"./servers/{dir}")
+        except OSError as exc:
+                if exc.errno == errno.EEXIST and os.path.isdir(file):
+                        print("dir already exists")
+                        sys.exit(1)
+
+        with open(f"./servers/{dir}/schema.json", "w+") as f:
+                f.writelines(json.dumps(data, indent=8))
+
+        with open(f"./servers/{dir}/schema", "w+") as f:
+                f.write(f"--u {url}\n")
+                for key in data["entities"].keys():
+                        f.write(f"- {key} {data["entities"][key]["type"]} {data["entities"][key]["arity"]}\n")
+
+
+def build_db(dir: str) -> None:
+        subprocess.run(["spacetime", "init", "--lang", "rust", "server"])
+
+
+
+def start_client(dir: str) -> None:
+        pass
+
+
 if __name__=='__main__':
-        args = sys.argv[:]
+        args = sys.argv[1:]
         print(args)
 
-        uFlag = args.find("-u")
-        fFlag = args.find("-f")
+        uFlag = "-u" in args
+        fFlag = "-f" in args
 
         url = ""
         file = ""
 
-        if uFlag < 0 and fFlag < 0:
+        # url and file for schema generation
+        # do not use url for copy
+
+        if not uFlag and not fFlag:
                 print("missing required arguments")
                 sys.exit(1)
 
-        if uFlag > 0 and uFlag + 1 >= len(args):
-                print("missing url after -u flag")
-                sys.exit(1)
-        elif uFlag > 0:
-                url = args[uFlag + 1]
-        else:
-                print("missing -u flag")
+        for idx, arg in enumerate(args):
+                if not arg in validArgs:
+                        continue
+                elif arg == "-u" and len(args) > idx + 1 and args[idx + 1] not in validArgs:
+                        url = args[idx + 1]
+                elif arg == "-f" and len(args) > idx + 1 and args[idx + 1] not in validArgs:
+                        file = args[idx + 1]
+                else:
+                        print(f"missing required arg after {arg}")
 
-        if fFlag > 0 and fFlag + 1 >= len(args):
-                print("missing file after -u flag")
-                sys.exit(1)
-        elif fFlag > 0:
-                url = args[fFlag + 1]
 
-        if not file:
-                # run file gen
-                print(f"base file generated at {url}.json")
-                sys.exit(0)
-        else:
-                with open(file, "r") as f:
-                        data = f.readlines()
-                data = [entry.strip() for entry in data if not entry.startswith("-")]
+        if file and not url:
+                # with open(file, "r") as f:
+                #         data = f.readlines()
+                # data = [entry.strip() for entry in data if not entry.startswith("-")]
 
-                ### create server
-                # run STDB commands for server
-                with open() as f:
-                        pass
+                # ### create server
+                # # run STDB commands for server
+                # with open() as f:
+                #         pass
 
-                ### create client
-                # run STDB commands for client
-                with open() as f:
-                        pass
+                # ### create client
+                # # run STDB commands for client
+                # with open() as f:
+                #         pass
 
                 print("generated output files at")
+        else:
+                # run file gen
+                init_schema(url, file)
+                sys.exit(0)
